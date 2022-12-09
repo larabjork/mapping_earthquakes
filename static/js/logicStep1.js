b6ed1e157c6e325a2afbd75aaf3202bc9ddbf879
larabjork <lara.m.bjork@gmail.com>
2022-12-09 14:56:44 -0800

    add initial files / exercise for EQ data from USGS

--- static/js/logicStep1.js
+++ static/js/logicStep1.js
@@ -1,13 +1,5 @@
 console.log("working");
 
-// Create satellite tile layer
-let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/satellite-v9',
-    accessToken: API_KEY
-});
-
 // Create satellite-streets tile layer
 let satelliteStreets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -27,22 +19,19 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 
 // Create base layer that holds both maps
 let baseMaps = {
-    "Satellite": satellite,
-    "Satellite Streets": satelliteStreets,
+    "Satellite": satelliteStreets,
     "Streets": streets
 };
 
 // Center map on Toronto, default to street layer
 let map = L.map("mapid", {
-    center: [43.7, -79.3],
-    zoom: 11,
+    center: [39.5, -98.5],
+    zoom: 3,
     layers: [streets]
 });
 
 L.control.layers(baseMaps).addTo(map);
 
-
-let torontoHoods = "https://raw.githubusercontent.com/larabjork/mapping_earthquakes/main/torontoNeighborhoods.json"
 
 let myStyle = {         
     fillColor: "#ffffa1",
@@ -50,14 +39,15 @@ let myStyle = {
     color: "#3366ff"
 };
 
-d3.json(torontoHoods).then(function(data){
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data){
     console.log(data);
-    L.geoJSON(data, {
-        style: myStyle,
-        onEachFeature: function(feature, layer) {
-                    layer.bindPopup("<h3>" + "Neighborhood: " + feature.properties.AREA_NAME+ "</h3>");
-                }
-    }
+    L.geoJSON(data
+    //     , {
+    //     style: myStyle,
+    //     onEachFeature: function(feature, layer) {
+    //                 layer.bindPopup("<h3>" + "Neighborhood: " + feature.properties.AREA_NAME+ "</h3>");
+    //             }
+    // }
         ).addTo(map)
  });
 

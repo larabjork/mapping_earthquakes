41345dc6817b66663a87e28dacc2bf8f92b2183e
larabjork <lara.m.bjork@gmail.com>
2022-12-09 14:28:09 -0800

    add Toronto neighborhood maps

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,69 +1,61 @@
 console.log("working");
 
-// Create light tile layer
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// Create satellite tile layer
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/light-v10',
+    id: 'mapbox/satellite-v9',
     accessToken: API_KEY
 });
 
-// Create dark tile layer
-let dark = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// Create satellite-streets tile layer
+let satelliteStreets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        id: 'mapbox/dark-v10',
+        id: 'mapbox/satellite-streets-v12',
         accessToken: API_KEY
     });
 
-
-// Create night navigation tile layer
-let nightNavigation = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// Create streets tile layer
+let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        id: 'mapbox/navigation-night-v1',
+        id: 'mapbox/streets-v12',
         accessToken: API_KEY
     });
 
-// Create day navigation layer
-let dayNavigation = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/navigation-day-v1',
-        accessToken: API_KEY
-    });
 
 // Create base layer that holds both maps
 let baseMaps = {
-    Light: light,
-    Dark: dark,
-    DayNavigation: dayNavigation,
-    NightNavigation: nightNavigation
+    "Satellite": satellite,
+    "Satellite Streets": satelliteStreets,
+    "Streets": streets
 };
 
-// Center map on Toronto, default to light layer
+// Center map on Toronto, default to street layer
 let map = L.map("mapid", {
-    center: [40, -80],
-    zoom: 2,
-    layers: [dark]
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [streets]
 });
 
 L.control.layers(baseMaps).addTo(map);
 
 
-let torontoData = "https://raw.githubusercontent.com/larabjork/mapping_earthquakes/main/torontoRoutes.json"
+let torontoHoods = "https://raw.githubusercontent.com/larabjork/mapping_earthquakes/main/torontoNeighborhoods.json"
 
 let myStyle = {         
-    color: "#ffffa1",
-    weight: 2
+    fillColor: "#ffffa1",
+    weight: 2,
+    color: "#3366ff"
 };
 
-d3.json(torontoData).then(function(data){
+d3.json(torontoHoods).then(function(data){
     console.log(data);
     L.geoJSON(data, {
         style: myStyle,
         onEachFeature: function(feature, layer) {
-                    layer.bindPopup("<h3>" + "Airline: " + feature.properties.airline+ "</h3>" + "<hr>" + "<h3>" + "Destination: " + feature.properties.dst +  "</h3>");
+                    layer.bindPopup("<h3>" + "Neighborhood: " + feature.properties.AREA_NAME+ "</h3>");
                 }
     }
         ).addTo(map)
